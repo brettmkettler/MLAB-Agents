6f1f3d11efe7a2d2f95da704118bdb25cd58d5fc
brettmkettler <brettkettler23@gmail.com>
2024-09-08 13:05:37 -0400

    updating the .gitignore file

--- AshAMQPApp/obj/Debug/net7.0/AshAMQPApp.AssemblyInfo.cs
+++ AshAMQPApp/obj/Debug/net7.0/AshAMQPApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AshAMQPApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1f68864bac7b0b4fdbac8e5e16f2958774da7a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("AshAMQPApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AshAMQPApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
